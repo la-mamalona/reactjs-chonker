e55b915339b62f0d28281f2b73cab080637611a9
MACS-KINCAID <97273014+MACS-KINCAID@users.noreply.github.com>
2022-01-28 19:44:03 -0600

    functional tiktaktoe

--- chonker/src/index.js
+++ chonker/src/index.js
@@ -31,14 +31,42 @@ class Board extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      squares : Array(9).fill(null)
+      squares : Array(9).fill(null),
+      banderaX : true,
+      winner: null
       //squares: ["X",null, "O", "O", "X", null, null, null, "X"] 
     }
   }
   handleClick(i){
+    if(this.state.squares[i] != null || this.state.winner != null){
+      return;
+    }
+
     let copia_squares = this.state.squares.slice();
-    copia_squares[i] = "X";
-    this.setState({squares : copia_squares});
+    /* V1
+    if(this.state.banderaX === true){
+      this.state.banderaX = false;
+      copia_squares[i] = "X";
+    }else{
+      this.state.banderaX = true;
+      copia_squares[i] = "O";
+    }
+    */
+
+    /*V2
+    if(this.state.banderaX === true){
+      copia_squares[i] = "X";
+    }else{
+      copia_squares[i] = "O";
+    }
+    this.state.banderaX = !this.state.banderaX;
+    */
+
+    copia_squares[i] = (this.state.banderaX === true ? "X" : "O");
+    //this.state.banderaX = !this.state.banderaX;
+
+    this.setState({squares : copia_squares, banderaX : !this.state.banderaX});
+    this.calculateWinner(copia_squares);
   }
 
   renderSquare(i) {
@@ -48,8 +76,45 @@ class Board extends React.Component {
     />;
   }
 
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    
+    lines.forEach(win_comb => {
+      //console.log("posición ", win_comb[0], " es ", squares[win_comb[0]], 
+      //    "// posición ", win_comb[1], " es ", squares[win_comb[1]],
+      //    "// posición ", win_comb[2], " es ", squares[win_comb[2]]);      
+
+      if(squares[win_comb[0]] != null 
+         && squares[win_comb[0]] === squares[win_comb[1]] 
+         && squares[win_comb[0]] === squares[win_comb[2]]
+      ){
+        //console.log("in", squares[win_comb[0]]);
+        //return squares[win_comb[0]];
+        this.setState({winner: squares[win_comb[0]]});
+      }
+      //console.log("out")
+      //return null;
+    });
+  
+  }
+
   render() {
-    const status = 'Next player: X';
+    //this.calculateWinner(this.state.squares);
+    let status;
+    if(this.state.winner){
+      status = "Winner is: " + this.state.winner;
+    }else{
+      status = 'Next player: ' + (this.state.banderaX === true ? "X" : "O");
+    }
 
     return (
       <div>
